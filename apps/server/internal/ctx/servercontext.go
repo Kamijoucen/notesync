bed68f9934cef8ca770d7e66ed4612cb93538afd
kamijoucen <kamijoucen@foxmail.com>
2024-10-24 00:54:21 +0800

    db

--- apps/server/internal/ctx/servercontext.go
+++ apps/server/internal/ctx/servercontext.go
@@ -1,13 +1,44 @@
 package ctx
 
-import "github.com/kamijoucen/notesync/pkg/config"
+import (
+	"context"
+	"log"
+
+	"entgo.io/ent/dialect"
+	"github.com/kamijoucen/notesync/pkg/config"
+	"github.com/kamijoucen/notesync/pkg/ent"
+	"github.com/kamijoucen/notesync/pkg/ent/migrate"
+
+	_ "github.com/mattn/go-sqlite3"
+)
 
 type ServerContext struct {
 	Config *config.ServerConfig
+	Ent    *ent.Client
 }
 
 func NewServerContext(cfg *config.ServerConfig) *ServerContext {
-	return &ServerContext{
+	svcCtx := &ServerContext{
 		Config: cfg,
 	}
+	svcCtx.initDB()
+	return svcCtx
+}
+
+// initDB
+func (s *ServerContext) initDB() {
+	// client, err := ent.Open(dialect.SQLite, "file:ent?mode=memory&cache=shared&_fk=1")
+	client, err := ent.Open(dialect.SQLite, "./data.db")
+	if err != nil {
+		log.Fatalf("failed opening connection to sqlite: %v", err)
+		panic(err)
+	}
+	// Run the auto migration tool.
+	if err := client.Schema.Create(context.Background(),
+		migrate.WithDropIndex(true),
+	); err != nil {
+		log.Fatalf("failed creating schema resources: %v", err)
+		panic(err)
+	}
+	s.Ent = client
 }
--- pkg/ent/schema/repository.go
+++ pkg/ent/schema/repository.go
@@ -1,6 +1,11 @@
 package schema
 
-import "entgo.io/ent"
+import (
+	"time"
+
+	"entgo.io/ent"
+	"entgo.io/ent/schema/field"
+)
 
 // Repository holds the schema definition for the Repository entity.
 type Repository struct {
@@ -9,7 +14,14 @@ type Repository struct {
 
 // Fields of the Repository.
 func (Repository) Fields() []ent.Field {
-	return nil
+	return []ent.Field{
+		field.String("name"),
+		field.String("description"),
+		field.String("path"),
+		field.Int("size"),
+		field.Time("created_at").Default(time.Now).Immutable(),
+		field.Time("updated_at").Default(time.Now).UpdateDefault(time.Now),
+	}
 }
 
 // Edges of the Repository.
